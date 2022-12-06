ce50dce7ee69756833c7744a40af75b07477d552
QiaoHui2000 <101460361+QiaoHui2000@users.noreply.github.com>
2022-12-06 12:01:58 +0800

    Add files via upload

---
+++ DatabaseConfig.php
@@ -0,0 +1,13 @@
+<?php
+
+$HostName = "localhost:3307";
+$HostUser = "root";
+$HostPass = "";
+$DatabaseName = "condo";
+
+// Create connection
+/*$conn = mysqli_connect($server, $username, $password, $database);
+if($conn->connect_error){
+	die("Connection failed: ". $conn->connect_error);
+}*/
+?>
