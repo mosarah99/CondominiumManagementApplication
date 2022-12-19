574805e0313d06d7648f8cd4ad87225a692cd147
QiaoHui2000 <b032010020@student.utem.edu.my>
2022-12-19 20:00:51 +0800

    Second

--- app/src/main/java/ftmk/workshop2/Condominium_Management_Application/FacilityMaintenance.java
+++ app/src/main/java/ftmk/workshop2/Condominium_Management_Application/FacilityMaintenance.java
@@ -1,9 +1,13 @@
 package ftmk.workshop2.Condominium_Management_Application;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 
@@ -21,11 +25,16 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class FacilityMaintenance extends AppCompatActivity {
 
     Spinner spinnerFacility, spinnerTime;
     ImageButton btnBack;
+    EditText edTxtDate;
+
+
+    private DatePickerDialog datePicker;
 
     ArrayList<String> facilityList = new ArrayList<>();
     ArrayAdapter<String> facilityAdapter;
@@ -35,7 +44,38 @@ public class FacilityMaintenance extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facility_maintenance);
 
+        //Assign variable
+        edTxtDate = (EditText) findViewById(R.id.editTxtDate);
 
+        edTxtDate.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction() == MotionEvent.ACTION_UP){
+                    //Initialize variable
+                    final Calendar cldr = Calendar.getInstance();
+
+                    //Get day, month, year
+                    int day = cldr.get(Calendar.DAY_OF_MONTH);
+                    int month = cldr.get(Calendar.MONTH);
+                    int year = cldr.get(Calendar.YEAR);
+
+                    // date picker dialog
+                    datePicker = new DatePickerDialog(FacilityMaintenance.this,
+                            new DatePickerDialog.OnDateSetListener() {
+                                @Override
+                                public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                                      int dayOfMonth) {
+                                    edTxtDate.setText(year  + "-" + (monthOfYear + 1)
+                                            + "-" + dayOfMonth);
+                                }
+                            }, year,month, day);
+                    datePicker.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
+                    datePicker.show();
+
+                }
+                return false;
+            }
+        });
 
         //Get all Id's
         btnBack = (ImageButton) findViewById(R.id.btnBack);
@@ -80,5 +120,14 @@ public class FacilityMaintenance extends AppCompatActivity {
             }
         });
         requestQueue.add(jsonObjectRequest);
+
+        /*String facilitySpinner = spinnerFacility.getSelectedItem().toString();
+        String timeSpinner = spinnerTime.getSelectedItem().toString();*/
+
+
+
+
     }
+
+
 }
