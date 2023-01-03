58707d97d5e3dd98884dad1a1cd69d0e377066f1
Israt07 <82527970+Israt07@users.noreply.github.com>
2023-01-03 20:43:21 +0800

    Add files via upload

---
+++ php/login.php
@@ -0,0 +1,19 @@
+<?php
+include_once 'conn.php';
+
+$username = $_POST['username'];
+$pass = $_POST['password'];
+
+$query = "SELECT * FROM residents WHERE username = '$username' AND password = '$pass'";
+$result = mysqli_query($con,$query);
+if (mysqli_num_rows($result) > 0) {
+    $resp['code'] = "200";
+    $resp["msg"] = "Login Success!";
+}else{
+    $resp['code'] = "202";
+    $resp["msg"] = "Login Faild!";
+}
+    
+echo json_encode($resp);
+mysqli_close($con);
+?>
